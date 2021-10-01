fa366f71e7a290165cb6959a8b1d0b0ac2a18eda
luisedu99 <luis.santos748@yahoo.com>
2021-10-01 08:21:18 -0600

    Subiendo desarrollo de guia 7

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import * as React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './src/Navigation/Navigation';
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Navigation></Navigation>
+    </NavigationContainer>
+  );
+}
